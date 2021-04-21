d52851b2eae8ce8f2e5105e6186d3dda0ef91e7a
aldolarrain <a2016114029@estudiantes.upsa.edu.bo>
2021-04-20 20:01:04 -0400

    Agregar archivos de proyecto.

---
+++ apiAHLO/Models/Data.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace apiAHLO.Models
+{
+    public class Data
+    {
+        [Key]
+        public string Estudiante { get; set; }
+        [DataType(DataType.DateTime)]
+        [Required]
+        public DateTime HoraYFecha { get; set; }
+        [Required]
+        public string Temperatura { get; set; }
+    }
+}
